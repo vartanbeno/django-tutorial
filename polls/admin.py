6d7d358060268f13bfbd418b1819233bb654f989
vartanbeno <vartanbeno@gmail.com>
2018-07-25 13:56:23 -0400

    looks better with TabularInline

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 1
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -7,6 +12,8 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {'fields': ['question_text']}),
         ('Date Information', {'fields': ['pub_date']})
     ]
+    inlines = [ChoiceInLine]
 
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
